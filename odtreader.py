5fe5303764ff89a0449d0ffcb6fa4db50644b23d
Marco Garipoli <marcuslxxii@gmail.com>
2017-11-03 11:09:35 +0100

    Risolti 3 bachi; gestione delle virgolette singole e doppie inclitate; gestione di shift+invio

--- odtreader.py
+++ odtreader.py
@@ -2,20 +2,22 @@
 """
 Modulo che si propone di restituire, dato un file odt (basato su
 LibreOffice v 5.4.2.2), il solo testo contenuto, senza usare moduli
-aggiuntivi ma solo quello che mette a disposizione una installazione
-standard di Python 3. In particolare si usa solo zipfile.
-Si può usare questo modulo per leggere una serie di file odt (magari
-centinaia) per analizzare i dati contenuti e poter generare un report.
-Non si sarà quindi costretti ad installare un nuovo modulo, ma basta
-copiare questo file accando al proprio, in cui lo si importa con:
-from odt-reader import odt2str
+aggiuntivi ma solo quello che mette a disposizione un'installazione
+standard di Python 3. In particolare si usa solo il modulo zipfile.
+
+Si può usare questo modulo per leggere una serie di file odt (centinaia
+o più) per analizzare i dati contenuti e poter generare un report. Non
+si sarà quindi costretti ad installare un nuovo modulo, ma basta copiare
+questo file accando al proprio, in cui lo si importa con:
+from odtreader import odt2str
 e lo si richiama con:
 testo = odt2str(nome_file)
 oppure:
 testo = odt2str(nome_file, False)
 se si vogliono mantenere gli spazi speciali ('\xa0', creati su Writer
-con Ctrl+Shift+Space) e quindi non sostituirli con spazi normali, o
-anche:
+con Ctrl+Shift+Space) e quindi non sostituirli con spazi normali, e le
+virgolette (singole e doppie) "abbellite" e non sostuituirle con il
+semplice apice ' o i semplici doppi apici ", o anche:
 testo = odt2str(nome_file, True, '\r\n')
 se si vuole che l'andata a capo sia alla "maniera di Windows" ('\r' è
 alla "maniera di Mac"), altrimenti il default è '\n' che è "alla
@@ -23,7 +25,13 @@ maniera di Linux". In fase di analisi, bisogna sapere qual è questo
 carattere (indipendentemente dal SO). Quando serve specificarlo? Solo se
 si vuol salvare in un file di testo il contenuto di un file odt, allora
 bisogna specificare il carattere dell'andata a capo del SO in cui si
-aprirà quel file di testo.
+aprirà quel file di testo. In caso invece di sola analisi è preferibile
+lasciare il default "\n" poiché rende molto più semplice l'analisi (se
+infatti analizzo il testo carattere per carattere, è più semplice sapere
+che si sta andando a capo analizzando il solo carattere "\n" piuttosto
+che dover anche verificare il carattere successivo!).
+Si puù anche specificare quale carattere si vuole quando si incontra lo
+shift + invio. Se non si specifica, sarà adottato lo stesso di invio.
 
 Si può testare questo modulo richiamandolo dalla shell di Linux o dal
 prompt dei comandi di Windows e ponendo come parametri il percorso ed
@@ -31,11 +39,10 @@ i nomi dei file odt da leggere. In questo caso verranno importati anche
 sys e os (entrambi già presenti, come zipfile, nell'installazione
 standard di Python 3). Es.: odtreader.py esempio.odt
 
-ATTENZIONE: non fa differenza tra Invio e Shift+Invio, non supporta le
-tabelle, né elementi grafici diversi dai checkbox (riportati con "[ ]"
-quelli deselezionati e con "[X]" quelli selezionati) e radiobutton
-(riportati con "( )" quelli deselezionati e con "(X)" quelli
-selezionati).
+ATTENZIONE: non supporta le tabelle, né elementi grafici diversi dai
+checkbox (riportati con "[ ]" quelli deselezionati e con "[X]" quelli
+selezionati) e radiobutton (riportati con "( )" quelli deselezionati e
+con "(X)" quelli selezionati).
 
    Copyright ©2017 Marco Garipoli
    
@@ -63,11 +70,13 @@ selezionati).
 __author__ = 'Marco Garipoli marcuslxxii@gmail.com'
 
 
-__version__ = "$Revision: 000000000001 $"
+__version__ = "$Revision: 000000000002 $"
 # $Source$
 
 
-def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
+#def odt2str(nome_file, normalizza_spazi_virgolette=True, a_capo='\n') -> str:
+def odt2str(nome_file, normalizza_spazi_virgolette=True, 
+            invio='\n', shift_invio=None) -> str:
     """
     Legge il file odt passato (completo di path) e lo restituisce come
     testo semplice.
@@ -75,17 +84,24 @@ def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
     specificato/i nella chiamata (di default '\n').
     Qualunque segno grafico sarà totalemente ignorato, mentre invece le
     tabulazioni saranno riportate fedelmente, così come le righe vuote.
-    v1.0 (23/10/2017)
+    v1.0.1 (3/11/2017)
     
     :type nome_file: str
     :param nome_file: percorso e nome del file odt da leggere
     
-    :type a_capo: str
-    :param a_capo: carattere/i da usare per l'andata a capo dell'output
+    :type normalizza_spazi_virgolette: bool
+    :param normalizza_spazi_virgolette: se True, sostituisce gli spazi
+                                        speciali (Ctrl+Shift+Space)
+                                        con spazi normali e le virgolette
+                                        (sia singole che doppie) "belle"
+                                        con quelle "normali"
     
-    :type normalizza_spazi: bool
-    :param normalizza_spazi: se True, sostituisce gli spazi speciali
-                             (Ctrl+Shift+Space) con spazi normali
+    :type invio: str
+    :param invio: carattere/i corrispondenti a invio
+    
+    :type shift_invio: str
+    :param shift_invio: carattere/i corrispondenti a shift_invio; se è
+                        None allora sarà uguale a invio
     """
     
     import zipfile as zf
@@ -96,14 +112,26 @@ def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
         solo "xxx"
         :type testo: str
         """
+        trace = False
+        if 'Codice' in testo and 'Fiscale' in testo:
+            trace = True
+            print('\n' + testo)
+        
         nonlocal forms
+        nonlocal shift_invio
         sub_out = ''
         i_t = testo.find('<text:')
         i_d = testo.find('<draw:')
+        
+        if trace:
+            print('i_t={} - i_d={}'.format(i_t, i_d))
+        
         if i_t > -1 and (i_d == -1 or i_t < i_d):
             sub_out = testo[:i_t]
             if testo[i_t+6:].startswith('tab/>'):
                 sub_out += '\t' + pulisci_span(testo[i_t+11:])
+            elif testo[i_t+6:].startswith('line-break/>'):
+                sub_out += shift_invio + pulisci_span(testo[i_t+18:])
             elif testo[i_t+6:].startswith('span '):
                 i = testo.find('>', i_t+11) + 1
                 if testo[i-2] != '/':
@@ -129,6 +157,8 @@ def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
             sub_out = testo
         return sub_out
     
+    if shift_invio is None:
+        shift_invio = invio
     forms = {}  # Nomi e rappresentaz di eventuali checkbox e radiobutton
     if not nome_file.lower().endswith('.odt'):
         nome_file += '.odt'
@@ -139,7 +169,7 @@ def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
     if t != '':
         #t = t.replace('<text:tab/>', '\t')  # Va fatto prima dei find!
         i = t.find('<form:form')
-        if i > -1:
+        if i > -1:  # Raccolta informazioni su checkbox e radiobutton presenti
             j = t.find('</form:form>')
             forms_info = t[i:j]  # Info su eventuali checkbox e radiobutton
             i2 = forms_info.find('<form:')
@@ -187,16 +217,22 @@ def odt2str(nome_file, normalizza_spazi=True, a_capo='\n') -> str:
             if t[i-2] != '/':
                 j = t.find('</text:p>', i)  # fine testo
                 # Il testo interessato in questo passaggio sta in t[i:j]
-                out += pulisci_span(t[i:j]) + a_capo  # '\n'
+                #print('\nPrima della pulitura:\n' + t[i:j])
+                #z = len(out)
+                out += pulisci_span(t[i:j]) + invio  # '\n'
+                #print('Dopo della pulitura:\n' + out[z:], end='')
                 i = t.find('<text:p', j+7)
             else:
-                out += a_capo  # '\n'
+                out += invio  # '\n'
                 i = t.find('<text:p', i)
-    out = out.replace('&lt;', '<')
-    out = out.replace('&gt;', '>')
-    if normalizza_spazi:
-        out = out.replace('\xa0', ' ')  # '\xa0'=chr(160): spazio "appiccicato"
-                                        # ' '   =chr( 32): spazio normale
+    # Correzione caratteri speciali di xml
+    out = (out.replace('&lt;', '<').replace('&gt;', '>')
+              .replace('&apos;', "'").replace('&quot;', '"')
+              .replace('&amp;', '&'))  # "&amp;" deve essere l'ultima sostituz!
+    if normalizza_spazi_virgolette:
+        out = (out.replace('\xa0', ' ')  # '\xa0'=chr(160):spazio "appiccicato"
+                  .replace(chr(8216), "'").replace(chr(8217), "'")   # ‘ ’ -> '
+                  .replace(chr(8220), '"').replace(chr(8221), '"'))  # “ ” -> "
     return out
 
 
@@ -204,11 +240,11 @@ def main():
     import sys, os
     
     num_param = len(sys.argv)  # sys.argv[0] è il nome del file .py
+    a_capo = '\n'  # Nel prompt dei comandi di Windows serve questo!
     if num_param > 1:
-        a_capo = os.linesep  # Usa il separatore del S.O.
         particolareggiata = False
         for i in list(range(1, num_param))[:]:
-            if sys.argv[i] == '-p':
+            if sys.argv[i] == '-c':
                 particolareggiata = True
                 num_param -= 1
             else:
@@ -219,24 +255,33 @@ def main():
                     print('-'*79)
                 if particolareggiata:
                     particolareggiata = False  # -p si specifica per ogni file
-                    t = odt2str(sys.argv[i], False)
+                    invio = '\n'
+                    shift_invio = '\r'
+                    t = odt2str(sys.argv[i], False, invio, shift_invio)
                     j = 1
                     for c in t:
-                        if c != '\n':
-                            print('"{}" -> ascii({})'.format(c, ord(c)),
+                        if c == invio:
+                            print('Invio')
+                            j = 0
+                        elif c == shift_invio:
+                            print('Shift+Invio')
+                            j = 0
+                        else:
+                            t = c
+                            if c == '\t':
+                                t = '\\t'
+                            print('"{}" -> ascii({})'.format(t, ord(c)),
                                                              end='\t')
                             if j%3 == 0:
                                 print()
-                        else:
-                            j = 0
-                            print('"[Invio]" -> ascii({})'.format(ord(c)))
                         j += 1
                     print()
                 else:
                     print(odt2str(sys.argv[i], True, a_capo))
     else:
         print("Indicare il/i file odt leggere.")
-        print("Usare l'opzione -p per avere una stampa particolareggiata.")
+        print("L'opzione -c renderà dettagliata (carattere per carattere) la "+
+              "stampa del file" + a_capo + "che la segue.")
 
 
 if __name__ == "__main__":
